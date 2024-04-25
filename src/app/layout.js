c5b3c6b95fd2717af8c4d3d96e5a5f0b5467c0c4
amitabhRazorpay <amitabh.baruah@razorpay.com>
2024-04-25 23:29:57 +0530

    basic setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const LatoFont = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"], // Add '400' for regular and '600' for semibold
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={LatoFont.className}>{children}</body>
     </html>
   );
 }
